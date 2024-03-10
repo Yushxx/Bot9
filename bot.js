6d46221073b7d326f61c2929e34f9823f1ce7eeb
Yushxx <109989261+Yushxx@users.noreply.github.com>
2024-03-10 09:33:52 +0000

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,55 @@
+const TelegramBot = require('node-telegram-bot-api');
+const token = 'YOUR_TELEGRAM_BOT_TOKEN';
+const bot = new TelegramBot(token, { polling: true });
+
+bot.onText(/\/start/, (msg) => {
+    bot.sendMessage(msg.chat.id, 'Bienvenue dans la bibliothèque des applications hack!', {
+        reply_markup: {
+            inline_keyboard: [
+                [
+                    { text: "Télécharger l'apk ⬇️", callback_data: 'download_apk' }
+                ]
+            ]
+        }
+    });
+});
+
+bot.on('callback_query', (callbackQuery) => {
+    const chatId = callbackQuery.message.chat.id;
+    const messageId = callbackQuery.message.message_id;
+
+    if (callbackQuery.data === 'download_apk') {
+        bot.sendMessage(chatId, 'Pour obtenir l\'application, veuillez créer un compte 1xbet authentique en utilisant le code promo Free221. Une fois que c\'est fait, cliquez sur le bouton suivant ➡️', {
+            reply_markup: {
+                inline_keyboard: [
+                    [
+                        { text: 'Suivant ➡️', callback_data: 'next' }
+                    ]
+                ]
+            }
+        });
+    }
+
+    if (callbackQuery.data === 'next') {
+        bot.sendMessage(chatId, 'Veuillez entrer votre ID pour vous connecter à l\'application.');
+    }
+});
+
+bot.on('message', (msg) => {
+    const chatId = msg.chat.id;
+    const userId = msg.text;
+
+    if (!isNaN(userId) && parseInt(userId) >= 700000000 && parseInt(userId) <= 999999999) {
+        bot.sendMessage(chatId, 'ID accepté. Voici le lien de téléchargement : https://t.me/SOLKAH00/5679', {
+            reply_markup: {
+                inline_keyboard: [
+                    [
+                        { text: 'Contact admin', url: 'https://t.me/Medatt00' }
+                    ]
+                ]
+            }
+        });
+    } else {
+        bot.sendMessage(chatId, 'Veuillez entrer un ID valide (entre 700000000 et 999999999).');
+    }
+});
