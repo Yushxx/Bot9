db271ff931816a13eac0d8bfb439ef45aabec9c6
Yushxx <109989261+Yushxx@users.noreply.github.com>
2024-03-10 11:41:05 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,5 @@
 const TelegramBot = require('node-telegram-bot-api');
+const http = require('http');
 const token = '6837924024:AAHYKYDukRPamhb-Wj8P6b_i3nnmVO-OPTA';
 const bot = new TelegramBot(token, { polling: true });
 
@@ -53,3 +54,17 @@ bot.on('message', (msg) => {
         bot.sendMessage(chatId, 'Veuillez entrer un ID valide (entre 700000000 et 999999999).');
     }
 });
+
+
+// Créez un serveur HTTP simple qui renvoie "I'm alive" lorsque vous accédez à son URL
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.write("I'm alive");
+    res.end();
+});
+
+// Écoutez le port 8080
+server.listen(8080, () => {
+    console.log("Keep alive server is running on port 8080");
+});
+
